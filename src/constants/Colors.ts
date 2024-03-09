74a9f5b8612494a36f6809ae7d8cf0e501f01376
khalilSa <khalilsa01@gmail.com>
2024-03-09 08:02:20 -0800

    Added Bottom Navigation Bar

---
+++ src/constants/Colors.ts
@@ -0,0 +1,21 @@
+const eminence = '#631D76';
+const charcoalGray = '#2E2532';
+const white = '#FBFBFB';
+const raisenBlack = '#201A23';
+
+export default {
+  light: {
+    text: raisenBlack,
+    background: white,
+    tint: eminence,
+    tabIconDefault: charcoalGray,
+    tabIconSelected: eminence,
+  },
+  dark: {
+    text: white,
+    background: charcoalGray,
+    tint: white,
+    tabIconDefault: eminence,
+    tabIconSelected: white,
+  },
+};
