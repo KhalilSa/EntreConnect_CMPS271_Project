a60213ec9ac040de381f240bb9a85ad818356fc4
gg0099 <115505741+gg0099@users.noreply.github.com>
2024-04-25 08:01:03 +0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDijVK9_L3-yFe0J1DiSjbjI7goYqYocxE",
+    authDomain: "test3-31ef5.firebaseapp.com",
+    projectId: "test3-31ef5",
+    storageBucket: "test3-31ef5.appspot.com",
+    messagingSenderId: "822688839726",
+    appId: "1:822688839726:web:7a18760e1a799de49f07bf",
+    measurementId: "G-MLRK27EZ25",
+  databaseURL: "https://test3-31ef5-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
+
